e9d11b880306010b06ea4e7d823630fb50551fc1
Anki2004 <ankitsharma082004@gmail.com>
2024-02-15 19:25:05 +0530

    Prediction Pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -72,7 +72,7 @@ class ModelTrainer:
             predicted = best_model.predict(X_test)
             r2_square = r2_score(y_test, predicted)
 
-            return r2_square
+            return r2_square 
 
         except Exception as e:
             raise CustomException(e,sys)
