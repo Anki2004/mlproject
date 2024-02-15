a40ac49a2099420ce40d33cdc04ccaeba787252b
Anki2004 <ankitsharma082004@gmail.com>
2024-02-15 18:28:06 +0530

    Model Trainer

--- src/components/data_ingestino.py
+++ src/components/data_ingestino.py
@@ -7,6 +7,9 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
     train_data_path:str = os.path.join('artifacts',"train.csv")
@@ -22,7 +25,7 @@ class DataIngestion:
         try:
             df = pd.read_csv('notebook\data\stud.csv')
             logging.info('Read the data as Dataframe')
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path))
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok = True)
             
             df.to_csv(self.ingestion_config.raw_data_path,index = False, header = True)
             logging.info("Train test split inittiated")
@@ -44,5 +47,8 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
 
-    data_transformation = DataTranformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    data_transformation = DataTransformation()
+    train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
